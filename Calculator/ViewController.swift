c9dc9f1b2c53083923cba3dcc3f8c9710ce84605
Chris Kelly <chrik@campus-024-140.ucdavis.edu>
2018-04-30 10:55:10 -0700

    Create links from buttons to functions. Give buttons tags. Give NumberPress some functionality. Still needs limits on number of decimals and length display and a way to handle numbers longer that 15 chars long.r

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        display.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var placeHolder : String = ""
 
-
+    @IBAction func NumberPress(_ sender: UIButton) {
+        var char : String = "\(sender.tag)"
+        if char == "10" {
+            char = "."
+        }
+        if display.text == "0" {
+            display.text = char
+        }
+        else{
+            display.text! += char
+        }
+    }
+    
+    @IBAction func UniaryOp(_ sender: UIButton) {
+        display.text = "0"
+    }
+    
+    @IBAction func BiOp(_ sender: UIButton) {
+        
+    }
+    
 }
 
